b6b47aa5d77b5a8a3a939c7ddd6966e102eb226a
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-05-16 09:17:58 -0300

    atividade 3 finalizada

---
+++ Estruturas_de_Repetição_Vetores_e_Matrizes.py
@@ -0,0 +1,18 @@
+n = int(input("Digite um número inteiro n: "))
+
+soma_impares = 0
+i = 1
+
+while i <= n:
+    soma_impares += i
+    i += 2 
+
+print(f"A soma dos números ímpares de 1 a {n} é: {soma_impares}")
+
+contador_pares_multiplos_3 = 0
+
+for j in range(1, n + 1):
+    if j % 2 == 0 and j % 3 == 0: 
+        contador_pares_multiplos_3 += 1
+
+print(f"O número de números pares múltiplos de 3 entre 1 e {n} é: {contador_pares_multiplos_3}")
